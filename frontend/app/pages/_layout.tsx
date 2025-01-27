996135c1d3c0c43ec750d9b81a4af15d9ce46777
Hirusha Dinujaya <74463840+hdx0315@users.noreply.github.com>
2025-01-27 21:48:19 +0530

    intro pages

---
+++ frontend/app/pages/_layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+const PagesLayout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="IntroOne"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="IntroTwo"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="IntroThree"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+      <StatusBar
+        backgroundColor="#065F46"
+        style="light"
+      />
+    </>
+  )
+}
+export default PagesLayout
