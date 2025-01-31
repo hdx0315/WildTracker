79c929b11c1d4f75c03059297a254d836ebe8579
Hirusha Dinujaya <74463840+hdx0315@users.noreply.github.com>
2025-01-31 21:39:42 +0530

    auth v2

--- frontend/app/auth/index.tsx
+++ frontend/app/auth/index.tsx
@@ -1,7 +1,7 @@
 
 // app/auth/index.tsx
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ImageBackground ,StatusBar } from 'react-native';
 import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import img from '../../assets/images/authBack.jpg'
@@ -18,34 +18,37 @@ const AuthIndex = () => {
   };
 
   return (
-      <SafeAreaView className="h-full bg-emerald-50">
-        <ImageBackground source={img} resizeMode='cover' className='h-full'>
+      <SafeAreaView className="h-full bg-black">
+        <StatusBar backgroundColor="transparent" translucent={true} />
 
-      <View className="flex flex-col items-center pt-20 px-4">
-        <Text className="text-center text-3xl font-bold text-white ">
+
+        <ImageBackground source={img} resizeMode='cover' className='h-[70%]'>
+
+          </ImageBackground>
+      <View className="flex flex-col px-4 justify-end gap-y-4">
+        <Text className="text-center text-3xl font-bold text-zinc-400 pt-4">
           Welcome !
         </Text>
-        <View className="mt-10">
+        <View className="mt-0">
           <TouchableOpacity
             onPress={navigateToSignIn}
             className="bg-emerald-800 rounded-xl p-4 mb-4"
           >
-            <Text className="text-center text-white text-xl font-semibold">
+            <Text className="text-center text-zinc-300 text-xl font-semibold">
               Sign In
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={navigateToSignUp}
-            className="bg-gray-200 rounded-xl p-4"
+            className="bg-zinc-300 rounded-xl p-4"
           >
-            <Text className="text-center text-emerald-800 text-xl font-semibold">
+            <Text className="text-center text-emerald-700 text-xl font-semibold">
               Sign Up
             </Text>
           </TouchableOpacity>
         </View>
       </View>
       
-      </ImageBackground>
     </SafeAreaView>
   );
 };
