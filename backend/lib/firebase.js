2378b126cf09b87b3219d4c63cba2777301f14a5
Hirusha Dinujaya <74463840+hdx0315@users.noreply.github.com>
2025-02-07 20:18:06 +0530

    firebse

--- backend/lib/firebase.js
+++ backend/lib/firebase.js
@@ -5,12 +5,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlrXe3fnMsoHAnkdaqvpCMxoLLavQa0LY",
-  authDomain: "wildtracker-438e0.firebaseapp.com",
-  projectId: "wildtracker-438e0",
-  storageBucket: "wildtracker-438e0.firebasestorage.app",
-  messagingSenderId: "103186647108",
-  appId: "1:103186647108:web:693f36f2d11cedfb1c5411"
 };
 
 // Initialize Firebase
